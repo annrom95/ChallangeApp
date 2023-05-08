ad3bed318287ab5825d00317d00f713d1fda8caf
annrom95 <anna.romanowska95@o2.pl>
2023-05-08 17:31:18 +0200

    Praca Domowa z IF dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex1 = "kobieta";
+var sex2 = "mężczyzna";
+var age = 33;
+if ((sex1 == "kobieta") && (age > 30))
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("Kobieta powyżej 30 lat");
+}
+if ((name == "Ewa") && (age == 33))
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else
+{
+    Console.WriteLine("Malwina, lat 40");
+}
+if ((sex2 == "mężczyzna") && (age > 18))
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Pełnoletni mężczyzna");
+}

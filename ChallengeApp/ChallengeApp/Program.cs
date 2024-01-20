f298d0234cfdf11eee8ca550de993e5843fc6da8
annrom95 <anna.romanowska95@o2.pl>
2024-01-20 21:47:28 +0100

    praca domowa dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,72 @@
-﻿string name = "Ewa";
-string sex = "kobieta";
-int age1 = 25;
-int age2 = 33;
-if (sex == "kobieta" && age1 < 30)
+﻿int number = 1188;
+string numberInString = number.ToString();
+char[] numbers = numberInString.ToArray();
+var numbers0 = 0;
+var numbers1 = 0;
+var numbers2 = 0;
+var numbers3 = 0;
+var numbers4 = 0;
+var numbers5 = 0;
+var numbers6 = 0;
+var numbers7 = 0;
+var numbers8 = 0;
+var numbers9 = 0;
+
+foreach (char letter in numbers)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+   if (letter == '0')
+    {
+        numbers0++;
+    }
+   else if (letter == '1')
+    {
+        numbers1++;
+    }
+   else if (letter == '2')
+    {
+        numbers2++;
+    }
+   else if (letter == '3')
+    {
+        numbers3++;
+    }
+   else if (letter == '4')
+    {
+        numbers4++;
+    }
+   else if (letter == '5')
+    {
+        numbers5++;
+    }
+   else if (letter == '6')
+    {
+        numbers6++;
+    }
+   else if (letter == '7')
+    {
+        numbers7++;
+    }
+   else if (letter == '8')
+    {
+        numbers8++;
+    }
+   else if (letter == '9')
+    {
+        numbers9++;
+    }
 }
-else
-{
-    Console.WriteLine("Kobieta powyżej 30 lat.");
-}
-if (name == "Ewa" && age2 == 33)
-{
-    Console.WriteLine("Ewa, lat 33.");
-}
-else if (sex == "mężczyzna" && age1 > 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+Console.WriteLine("0=>" + numbers0);
+Console.WriteLine("1=>" + numbers1);
+Console.WriteLine("2=>" + numbers2);
+Console.WriteLine("3=>" + numbers3);
+Console.WriteLine("4=>" + numbers4);
+Console.WriteLine("5=>" + numbers5);
+Console.WriteLine("6=>" + numbers6);
+Console.WriteLine("7=>" + numbers7);
+Console.WriteLine("8=>" + numbers8);
+Console.WriteLine("9=>" + numbers9);
+
+
+
+
+    
